a33da2855e0ccf823150cbd8c442b58c188e788a
crystalee01 <crystallee10151@gmail.com>
2018-07-11 22:19:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Write your code in this file!
-
+function getUKTime(){
+  var time = new Date().getHours() + 5;
+  return time;
+}
+function discountTicket(price){
+  return price * 0.8;
+}
+function tweetReply(fanName){
+  return `Hey ${fanName}, thanks so much for the support! I really appreciate it!`;
+}
 
