d3129c5a88a3f7d34e2cfce0268c9878aa7cdc27
kmelamed <kmelamed@buckley.org>
2018-07-11 22:24:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Write your code in this file!
+function getUKTime() {
+  var UKTime = new Date().getHours()+8
+  return UKTime
+}
 
+function discountTicket(x) {
+  return x * .8;
+}
 
+function tweetReply(name) {
+  return "Hey " + name + ", thanks so much for the support! I really appreciate it!"
+}
